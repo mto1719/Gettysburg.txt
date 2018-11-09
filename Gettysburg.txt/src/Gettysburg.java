8d707056b471f54b218e02369426cfa33302602a
mto1719 <42977020+mto1719@users.noreply.github.com>
2018-11-09 07:27:24 -0500

    Update Gettysburg.java

--- Gettysburg.txt/src/Gettysburg.java
+++ Gettysburg.txt/src/Gettysburg.java
@@ -1,9 +1,27 @@
 
 public class Gettysburg {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args);{
+    int count = 0;
+    double sum = 0;
+    double average = 0;
 
-	}
+    while (sc.hasNext()) {
+
+        String userInput = sc.next();
+
+        double charNum = userInput.length();
+        sum = charNum + sum;
+        count++;
+
+        double average = 0;
+        if (count > 0) {
+            average = sum / count;
+        }
+
+
+        System.out.println("Average word length = " + average);
+
+        }
 
 }
