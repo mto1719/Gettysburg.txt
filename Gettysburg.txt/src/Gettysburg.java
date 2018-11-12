ea88b64cfa431345a8aaecb7b32164404abfbfd1
mto1719 <42977020+mto1719@users.noreply.github.com>
2018-11-12 13:37:39 -0500

    Update Gettysburg.java

--- Gettysburg.txt/src/Gettysburg.java
+++ Gettysburg.txt/src/Gettysburg.java
@@ -1,37 +1,41 @@
-
+/*
+ * Matthew To
+ */
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 public class Gettysburg {
 
-public static void main(String[] args);
+public static void main(String[] args) throws FileNotFoundException
     {
-     ArrayList<String> GettysburgAddress = new ArrayList<String>();
-     Scanner file = new Scanner(new File(GettysburgAddress));
-      while (file.hasNext()) 
-	{
-	 word = file.next();
-	 } 
-    int count = 0;
+	int count = 0;
     double sum = 0;
     double average = 0;
-
-    while (file.hasNext()) {
-
+	ArrayList<String> word = new ArrayList<String>();​
+	Scanner file = new Scanner(new File("S:\\git\\GettysburgA.txt\\Gettysburg.txt\\src\\Gettysburg"));
+	while (file.hasNext()) 
+	{
+	word = file.next();
+	}  
+	int wordL = word.length();
+    while (file.hasNext()) 
+    {
         String userInput = file.next();
-
         double charNum = userInput.length();
         sum = charNum + sum;
         count++;
-        if (count > 0) {
+        if (count > 0) 
+        {
             average = sum / count;
         }
-	int largestString = Str.get(0).length();
-	int index = 0;
-	if(Str.get(i).length() > largestString) 
+	int largestString = wordL;
+	if(wordL > largestString) 
 	{
-        largestString = Str.get(i).length();
-        index = i;
-        }
-        System.out.println("Average word length = " + average);
-	System.out.println("Index " + index + " "+ Str.get(index) + " " + "is the largest and is size " + largestString);
-        }
-
+        largestString = wordL;
+    }
+    System.out.println("Average word length = " + average);
+	System.out.println("the largest word is " + largestString);
+    }
+}
 }
